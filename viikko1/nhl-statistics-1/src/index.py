a9b56615a08220cd1fc0b845b20d0996cbfdd1e2
l-antti <laura.k.anttila@gmail.com>
2024-11-04 21:31:52 +0200

    Reorganize URL fetch

--- viikko1/nhl-statistics-1/src/index.py
+++ viikko1/nhl-statistics-1/src/index.py
@@ -2,7 +2,8 @@ from statistics_service import StatisticsService
 
 
 def main():
-    stats = StatisticsService()
+    player_reader = PlayerReader("https://studies.cs.helsinki.fi/nhlstats/2022-23/players.txt")
+    stats = StatisticsService(player_reader)
     philadelphia_flyers_players = stats.team("PHI")
     top_scorers = stats.top(10)
 
--- viikko1/nhl-statistics-1/src/statistics_service.py
+++ viikko1/nhl-statistics-1/src/statistics_service.py
@@ -3,10 +3,8 @@ from player_reader import PlayerReader
 
 
 class StatisticsService:
-    def __init__(self):
-        reader = PlayerReader()
-
-        self._players = reader.get_players()
+    def __init__(self, player_reader):
+        self._players = player_reader.get_players()
 
     def search(self, name):
         for player in self._players:
